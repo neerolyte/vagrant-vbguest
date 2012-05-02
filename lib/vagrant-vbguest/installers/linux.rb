7d2e25a741264258735e1f9b4e11a4a9b263d7da
Robert Schulze <robert@dotless.de>
2012-05-02 18:57:41 +0200

    using attribute

--- lib/vagrant-vbguest/installers/linux.rb
+++ lib/vagrant-vbguest/installers/linux.rb
@@ -21,7 +21,7 @@ module VagrantVbguest
       end
 
       def install(iso_file, opts=nil, &block)
-        @vm.ui.warn I18n.t("vagrant.plugins.vbguest.generic_linux_installer")
+        vm.ui.warn I18n.t("vagrant.plugins.vbguest.generic_linux_installer")
         upload(iso_file)
         vm.channel.sudo("mount #{tmp_path} -o loop #{mount_point}", opts, &block)
         vm.channel.sudo("#{mount_point}/VBoxLinuxAdditions.run --nox11", opts, &block)
