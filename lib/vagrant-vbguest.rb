2512621abdf854f88e056df2de10f840b4501470
Robert Schulze <robert@dotless.de>
2012-05-22 12:07:43 +0200

    using new `activated` block to require plugin files

--- lib/vagrant-vbguest.rb
+++ lib/vagrant-vbguest.rb
@@ -1,20 +1,3 @@
-require 'vagrant'
-require 'vagrant-vbguest/errors'
-
-require 'vagrant-vbguest/installer'
-require 'vagrant-vbguest/installers/base'
-require 'vagrant-vbguest/installers/linux'
-require 'vagrant-vbguest/installers/debian'
-require 'vagrant-vbguest/installers/ubuntu'
-
-require 'vagrant-vbguest/action'
-require 'vagrant-vbguest/config'
-require 'vagrant-vbguest/command'
-
-require 'vagrant-vbguest/detector'
-require 'vagrant-vbguest/download'
-
-
 # Add our custom translations to the load path
 I18n.load_path << File.expand_path("../../locales/en.yml", __FILE__)
 
@@ -26,6 +9,23 @@ module VagrantVbguest
     Provides automatic and/or manual management of the
     VirtualBox Guest Additions inside the Vagrant environment.
     DESC
+
+    activated do
+      require 'vagrant-vbguest/errors'
+
+      require 'vagrant-vbguest/installer'
+      require 'vagrant-vbguest/installers/base'
+      require 'vagrant-vbguest/installers/linux'
+      require 'vagrant-vbguest/installers/debian'
+      require 'vagrant-vbguest/installers/ubuntu'
+
+      require 'vagrant-vbguest/action'
+      require 'vagrant-vbguest/config'
+      require 'vagrant-vbguest/command'
+
+      require 'vagrant-vbguest/detector'
+      require 'vagrant-vbguest/download'
+    end
 
     config('vbguest') { Config }
     command('vbguest') { Command }
