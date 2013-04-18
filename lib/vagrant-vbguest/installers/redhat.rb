d35e4fc7a9ff67610ce89f0688043985c3d85c89
David Schoen <dave@lyte.id.au>
2013-04-19 09:24:42 +1000

    Trying to upgrade kernel under EL6 derivitives
    Often the kernel-devel package for the running kernel will be missing
    from the repos if the running kernel is out of date.
    
    This attempts to upgrade the kernel if kernel-devel is not available at
    the right version.
    
    Currently it *does* work, but results in the VM getting the
    VBoxGuestAdditions iso copied to the VM and installing them twice - which may
    be something to do with the forced reboot for the kernel upgrade.

--- lib/vagrant-vbguest/installers/redhat.rb
+++ lib/vagrant-vbguest/installers/redhat.rb
@@ -1,6 +1,8 @@
 module VagrantVbguest
   module Installers
     class RedHat < Linux
+      include VagrantVbguest::Helpers::Rebootable
+
       # Scientific Linux (and probably CentOS) both show up as :redhat
       # fortunately they're probably both similar enough to RHEL
       # (RedHat Enterprise Linux) not to matter.
@@ -8,13 +10,35 @@ module VagrantVbguest
         :redhat == self.distro(vm)
       end
 
-      # Install missing deps and yield up to regular linux installation
       def install(opts=nil, &block)
-        communicate.sudo(install_dependencies_cmd, opts, &block)
+        # do simple deps first
+        install_deps(opts, &block)
+        check_and_upgrade_kernel_devel(opts, &block)
+        # yield to regular tools installation
         super
       end
 
     protected
+      def install_deps(opts=nil, &block)
+        communicate.sudo(install_dependencies_cmd, opts, &block)
+      end
+
+      def check_and_upgrade_kernel_devel(opts=nil, &block)
+        check_opts = {:error_check => false}.merge(opts || {})
+        exit_status = communicate.sudo("rpm -q kernel-devel-`uname -r`", check_opts, &block) 
+
+        if exit_status == 1 then
+          upgrade_kernel(opts, &block)
+        end
+      end
+
+      def upgrade_kernel(opts=nil, &block)
+        @env.ui.warn("Attempting to upgrade the kernel as the right version of kernel-devel is missing from yum repos")
+        communicate.sudo("yum install -y kernel{,-devel}", opts, &block)
+        @env.ui.warn("Restarting to activate upgraded kernel so that VirtualBox Guest Tools installation can continue")
+        reboot(@vm, {:auto_reboot => true})
+      end
+
       def install_dependencies_cmd
         "yum install -y #{dependencies}"
       end
